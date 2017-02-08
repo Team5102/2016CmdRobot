1780f2c1400fda0ac852ba4ba6e887cfb9f39360
Luke Lopez <hurricanelukeg.l@gmail.com>
2017-02-07 18:32:47 -0800

    Ur welcome ;)

--- src/org/usfirst/frc/team5102/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team5102/robot/subsystems/Drive.java
@@ -1,26 +1,32 @@
 package org.usfirst.frc.team5102.robot.subsystems;
 
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import org.usfirst.frc.team5102.robot.RobotMap;
+import org.usfirst.frc.team5102.robot.subsystems.MultiSpeedController;
 
 /**
  *
  */
 public class Drive extends Subsystem 
 {
-	public static RobotDrive robotDrive;
 
 	
-	leftDriveMotors = new MultiSpeedController(
+	static MultiSpeedController leftDriveMotors = new MultiSpeedController(
 			new Talon(RobotMap.leftDriveMotor1),
 			new Talon(RobotMap.leftDriveMotor2),
-			new Talon(RobotMap.leftDriveMotor3));
+			new Talon(RobotMap.leftDriveMotor3)
+		);
 	
-	rightDriveMotors = new MultiSpeedController(
+	static MultiSpeedController rightDriveMotors = new MultiSpeedController(
 			new Talon(RobotMap.rightDriveMotor1),
 			new Talon(RobotMap.rightDriveMotor2),
-			new Talon(RobotMap.rightDriveMotor3));
-	robotDrive = new RobotDrive(leftDriveMotors, rightDriveMotors);
+			new Talon(RobotMap.rightDriveMotor3)
+		);
+	
+	public static RobotDrive robotDrive  = new RobotDrive(leftDriveMotors, rightDriveMotors);
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
--- src/org/usfirst/frc/team5102/robot/subsystems/MultiSpeedController.java
+++ src/org/usfirst/frc/team5102/robot/subsystems/MultiSpeedController.java
@@ -1,18 +1,74 @@
 package org.usfirst.frc.team5102.robot.subsystems;
+import edu.wpi.first.wpilibj.SpeedController;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
+public class MultiSpeedController implements SpeedController
+{
+    private SpeedController[] speedControllers;
+    private double speed;
 
-/**
- *
- */
-public class MultiSpeedController extends Subsystem {
-
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+    public MultiSpeedController(SpeedController... speedControllers)
+    {
+        this.speedControllers = speedControllers;
+        this.set(0.0);
     }
-}
 
+    @Override
+    public double get()
+    {
+        return this.speed;
+    }
+
+    @Override
+    public void set(double speed)
+    {
+        this.speed = speed;
+
+        for (SpeedController speedController : this.speedControllers)
+        {
+            speedController.set(speed);
+        }
+    }
+
+    //@Override
+    public void set(double speed, byte syncGroup)
+    {
+        this.set(speed);
+    }
+
+    @Override
+    public void pidWrite(double output)
+    {
+        this.set(output);
+    }
+
+    @Override
+    public void disable()
+    {
+        for (SpeedController speedController : this.speedControllers)
+        {
+            speedController.disable();
+        }
+    }
+
+	@Override
+	public void setInverted(boolean isInverted)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean getInverted()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public void stopMotor()
+	{
+		for (SpeedController speedController : this.speedControllers)
+        {
+            speedController.set(0.0);
+        }
+	}
+}
